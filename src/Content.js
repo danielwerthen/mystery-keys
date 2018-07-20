67e1bd85393d73e77ffed1b0c91c5cccdf225c8d
Daniel Werthen <danielwerthen@gmail.com>
2018-07-20 11:27:27 +0200

    Add preview mode

--- src/Content.js
+++ src/Content.js
@@ -4,10 +4,15 @@ const contentful = require('contentful');
 
 const token =
   '0fa59bb788bc44383340208358280d558743902d8c6f15ebaf2e5b23e0e6f9a9';
+const previewToken =
+  'fa91a5528a314b16f9a7afbac6cadcfd8e74ce8d7525893263aad7a3a17044b6';
+
+const isPreview = window.location.search.indexOf('preview') > -1;
 
 const client = contentful.createClient({
   space: 'cf566xzj5m80',
-  accessToken: token,
+  accessToken: isPreview ? previewToken : token,
+  host: isPreview ? 'preview.contentful.com' : undefined,
 });
 
 export default class Content extends React.Component {
