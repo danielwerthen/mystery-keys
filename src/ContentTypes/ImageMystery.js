b151f9af25c1c68cc0b2f90c59bee1ea5e8acda6
Daniel Werthen <danielwerthen@gmail.com>
2018-07-20 11:18:15 +0200

    Image or video works

--- src/ContentTypes/ImageMystery.js
+++ src/ContentTypes/ImageMystery.js
@@ -10,15 +10,30 @@ function isMatching(a, b) {
 }
 
 export default function ImageMystery({ image, password, onValid }) {
+  const { file } = image.fields;
+  const isVideo = file.contentType.indexOf('video') > -1;
   return (
     <div>
-      <img
-        $display="block"
-        $margin="auto"
-        $maxWidth="90%"
-        src={image.fields.file.url}
-        alt={image.fields.title}
-      />
+      {isVideo ? (
+        <video
+          controls
+          muted
+          src={image.fields.file.url}
+          $display="block"
+          $margin="auto"
+          $maxWidth="90%"
+        >
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+      ) : (
+        <img
+          $display="block"
+          $margin="auto"
+          $maxWidth="90%"
+          src={image.fields.file.url}
+          alt={image.fields.title}
+        />
+      )}
       <input
         $background="#EEE"
         $border="1px solid #BBB"
